91c97f4ba00130182670d573e8527b460a824d71
Saturnno <ramiroae@live.com>
2019-12-26 00:40:51 -0700

    Agregado metodo para obtener Articulos por categoria

--- BeerApp.DAO/daoProductos.cs
+++ BeerApp.DAO/daoProductos.cs
@@ -50,5 +50,45 @@ namespace BeerApp.DAO
 
             return res;
         }
+
+        public strRespuesta ObtenerPorCategoria()
+        {
+            var objConn = Conexion.Instancia.getConexion();
+            SqlCommand objCmd = new SqlCommand("Catalogo_Obtener_Productos_Por_Categoria", objConn);
+            objCmd.CommandType = CommandType.StoredProcedure;
+            objCmd.CommandTimeout = 3600;
+            strRespuesta res = null;
+
+            List<strProductos> l = new List<strProductos>();
+            try
+            {
+                objConn.Open();
+                SqlDataReader lector = objCmd.ExecuteReader();
+                while (lector.Read())
+                {
+                    strProductos str = new strProductos();
+                    str.ID = int.Parse(lector["ID"].ToString());
+                    str.Nombre = lector["Nombre"].ToString();
+                    str.Imagen = lector["Imagen"].ToString();
+                    str.Categoria = int.Parse(lector["Categoria"].ToString());
+                    str.Precio = decimal.Parse(lector["Precio"].ToString());
+                    l.Add(str);
+                }
+                res = new strRespuesta() { IsError = false, Message = "", Productos = l };
+
+
+            }
+            catch (Exception e)
+            {
+                res = new strRespuesta() { IsError = true, Message = "Ha ocurrido un problema al traer los productos." };
+
+            }
+            finally
+            {
+                objConn.Close();
+            }
+
+            return res;
+        }
     }
 }
